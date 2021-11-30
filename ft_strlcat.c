e094b7ac4f2850484e80038a9f7b704c8abdb18e
Margaux RIANT <mriant@e2r4p10.clusters.42paris.fr>
2021-11-30 11:13:10 +0100

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,30 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mriant <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/26 17:54:48 by mriant            #+#    #+#             */
-/*   Updated: 2021/11/30 09:22:19 by mriant           ###   ########.fr       */
+/*   Created: 2021/11/30 09:39:39 by mriant            #+#    #+#             */
+/*   Updated: 2021/11/30 10:58:55 by mriant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
+	size_t	dst_len;
+	size_t	src_len;
 	size_t	i;
 
-	if (size > 0)
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (size <= dst_len)
+		return (size + src_len);
+	i = 0;
+	while (i < size - 1 && src[i])
 	{
-		i = 0;
-		while (i < size - 1)
-		{
-			dst[i] = src[i];
-			i ++;
-		}
-		dst[i] = '\0';
+		dst[i + dst_len] = src[i];
+		i ++;
 	}
-	return (ft_strlen(src));
+	dst[i + dst_len] = '\0';
+	return (dst_len + src_len);
 }
