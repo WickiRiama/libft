d233c29d6aa2ae2af151ca97e1e4094efe19e867
Margaux RIANT <mriant@e2r4p10.clusters.42paris.fr>
2021-12-02 10:53:22 +0100

    atoi

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: mriant <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/23 12:22:38 by mriant            #+#    #+#              #
-#    Updated: 2021/12/02 09:28:17 by mriant           ###   ########.fr        #
+#    Updated: 2021/12/02 10:41:37 by mriant           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -30,7 +30,8 @@ SRCS = ft_isalpha.c \
 	ft_strncmp.c \
 	ft_memchr.c \
 	ft_memcmp.c \
-	ft_strnstr.c
+	ft_strnstr.c \
+	ft_atoi.c
 
 OBJ = ${SRCS:%.c=%.o}
 FLAGS = -Wall -Wextra -Werror
---
+++ ft_atoi.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mriant <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/02 10:15:16 by mriant            #+#    #+#             */
+/*   Updated: 2021/12/02 10:52:09 by mriant           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_isspace(char c)
+{
+	if ((c == 32) || (c >= 9 && c <= 13))
+		return (1);
+	else
+		return (0);
+}
+
+int	ft_atoi(const char *nptr)
+{
+	long int	result;
+	int			ispos;
+	int			i;
+
+	i = 0;
+	ispos = 1;
+	result = 0;
+	while (ft_isspace(nptr[i]))
+		i ++;
+	if (nptr[i] == '+')
+		i ++;
+	else if (nptr[i] == '-')
+	{
+		i ++;
+		ispos = -1;
+	}
+	while (ft_isdigit(nptr[i]))
+	{
+		result = result * 10 + (nptr[i] - '0');
+		i ++;
+	}
+	result = ispos * result;
+	return ((int)result);
+}
