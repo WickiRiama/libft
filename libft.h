5e6c0bd535db00c7d8632f2de255f2c2435e02b8
Margaux RIANT <mriant@e2r3p10.clusters.42paris.fr>
2021-12-03 17:55:02 +0100

    putnbr_fd

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mriant <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 17:16:58 by mriant            #+#    #+#             */
-/*   Updated: 2021/12/03 17:31:07 by mriant           ###   ########.fr       */
+/*   Updated: 2021/12/03 17:51:43 by mriant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,5 +50,6 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
