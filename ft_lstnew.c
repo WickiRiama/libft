17e97614f28972208e24d09fe95406a7f7a3f5f4
Margaux RIANT <mriant@e2r5p9.clusters.42paris.fr>
2021-12-06 10:46:15 +0100

    lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: mriant <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 15:40:11 by mriant            #+#    #+#             */
-/*   Updated: 2021/12/04 16:59:53 by mriant           ###   ########.fr       */
+/*   Updated: 2021/12/06 10:38:52 by mriant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,27 +21,3 @@ t_list	*ft_lstnew(void *content)
 	new->next = NULL;
 	return (new);
 }
-
-/*
-#include <stdio.h>
-
-int	main()
-{
-	int	a;
-	char	c;
-	char	*str = "abcd";
-	t_list	*liste;
-
-	a = 4;
-	c = 'b';
-	liste = ft_lstnew(&a);
-	printf("%d\n", *(int *) liste->content);
-	free(liste);
-	liste = ft_lstnew(&c);
-	printf("%c\n", *(char *) liste->content);
-	free(liste);
-	liste = ft_lstnew(str);
-	printf("%s\n", (char *) liste->content);
-	free(liste);
-}
-*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mriant <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 17:16:58 by mriant            #+#    #+#             */
-/*   Updated: 2021/12/04 15:36:16 by mriant           ###   ########.fr       */
+/*   Updated: 2021/12/06 10:40:29 by mriant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,5 +56,6 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+t_list	*ft_lstnew(void *content);
 
 #endif
