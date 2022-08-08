a91d818182403b68db6fa1a871caa5359f04125e
Margaux Riant <mriant@student.42.fr>
2022-08-08 10:21:00 +0200

    Protect ft_strjoin in case of NULL pointer
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: mriant <mriant@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 17:38:19 by mriant            #+#    #+#             */
-/*   Updated: 2022/03/22 17:28:15 by mriant           ###   ########.fr       */
+/*   Updated: 2022/08/08 10:20:27 by mriant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,17 @@ char	*ft_strjoin(char const *s, char const *s2, char *sep)
 			(ft_strlen(s) + ft_strlen(sep) + ft_strlen(s2) + 1));
 	if (!result)
 		return (NULL);
-	while (s[i])
+	while (s && s[i])
 	{
 		result[i] = s[i];
 		i ++;
 	}
-	while (sep[i - ft_strlen(s)])
+	while (sep && sep[i - ft_strlen(s)])
 	{
 		result[i] = sep[i - ft_strlen(s)];
 		i ++;
 	}
-	while (s2[i - ft_strlen(s) - ft_strlen(sep)])
+	while (s2 && s2[i - ft_strlen(s) - ft_strlen(sep)])
 	{
 		result[i] = s2[i - ft_strlen(s) - ft_strlen(sep)];
 		i ++;
